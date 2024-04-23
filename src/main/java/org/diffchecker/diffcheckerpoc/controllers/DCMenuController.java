e1149650c9012f7f3f9193544d6f4635f1be30d9
Ravindranadh Inavolu <ravindranadh.inavolu@gmail.com>
2024-04-23 12:03:58 +0530

    Added a menu scene to navigate between different checkers

---
+++ src/main/java/org/diffchecker/diffcheckerpoc/controllers/DCMenuController.java
@@ -0,0 +1,42 @@
+package org.diffchecker.diffcheckerpoc.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DCMenuController implements Initializable {
+    public Button text_dc_btn;
+    public Button dir_dc_btn;
+    public Button api_dc_btn;
+    public Button audio_dc_btn;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        text_dc_btn.setOnAction(actionEvent -> textDiffCheckerButtonOnAction());
+        dir_dc_btn.setDisable(true);
+        api_dc_btn.setDisable(true);
+        audio_dc_btn.setDisable(true);
+    }
+
+    private void textDiffCheckerButtonOnAction() {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/diffchecker/diffcheckerpoc/dc-input-scene.fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Text Checker Poc");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
